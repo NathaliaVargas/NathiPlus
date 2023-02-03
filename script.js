67b4000b8addeedecad7e03e6ffee615db04ae27
Nathalia Vargas <65309796+NathaliaVargas@users.noreply.github.com>
2023-02-02 23:50:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var listaSeries = ['https://m.media-amazon.com/images/M/MV5BZTdkMGNhZmYtNzAxMy00NWJkLThkN2MtNjVkNzY0NzVlMWQ4XkEyXkFqcGdeQXVyMTA0MDgzNTE1._V1_FMjpg_UX1000_.jpg','https://br.web.img3.acsta.net/pictures/19/12/24/09/27/2970070.jpg','https://br.web.img3.acsta.net/pictures/22/10/05/09/42/3385930.jpg','https://noticiasdatv.uol.com.br/media/_versions/recomenda/this-is-us-poster-season-4_fixed_recomenda_big.jpg']
-var listaNomes = ['Designated Survivor', 'The Rings of Power', 'This is Us']
+var listaNomes = ['Designated Survivor', 'Manifest','The Rings of Power', 'This is Us']
 
 var listaFilmes = [
   'https://br.web.img2.acsta.net/pictures/22/06/03/13/41/4557063.jpg', 'https://www.curtocinema.com.br/wp-content/uploads/2022/01/um-lugar-silencioso-parte2.jpg', 'https://br.web.img3.acsta.net/pictures/20/12/02/16/22/0450183.jpg'
